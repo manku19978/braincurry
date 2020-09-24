e5d7c8955f113cdd16d307a31425ac8d3095f8f0
mayank19978 <mayanks1@dewsolutions.in>
2020-09-24 19:57:37 +0530

    Updates

---
+++ precache-manifest.bd1ae20d32648d950a3f423fb9cbbf0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2113de4a670cf3b32e22a126ab0d07",
+    "url": "/braincurry/index.html"
+  },
+  {
+    "revision": "6118ac9586d1eb051afb",
+    "url": "/braincurry/static/css/main.97c8601d.chunk.css"
+  },
+  {
+    "revision": "e8ac604a01e72f69408e",
+    "url": "/braincurry/static/js/2.500370db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/braincurry/static/js/2.500370db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6118ac9586d1eb051afb",
+    "url": "/braincurry/static/js/main.f0dcbf94.chunk.js"
+  },
+  {
+    "revision": "edd1a5172d149ae2dc7e",
+    "url": "/braincurry/static/js/runtime-main.b202a172.js"
+  }
+]);
